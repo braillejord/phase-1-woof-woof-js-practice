22a35f754d06af864441ce6cc80f1774f6e18655
BreElle Wells <breellejordyn@gmail.com>
2023-07-27 22:30:41 -0600

    send patch request for good/bad dog

--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@ const dogUrl = 'http://localhost:3000/pups'
 const dogNameDiv = document.getElementById('dog-bar')
 
 // step 2: add pups to dog bar
-fetch (dogUrl)
-.then(r => r.json())
-.then(data => renderDogNames(data))
+fetch(dogUrl)
+    .then(r => r.json())
+    .then(data => renderDogNames(data))
 
 function renderDogNames(dogs) {
     dogs.forEach((singleDog) => {
@@ -36,5 +36,30 @@ function renderDogInfo(dog) {
         dogButton.innerText = 'Bad dog!'
     }
 
+    dogButton.onclick = () => {
+        if (dogButton.innerText === 'Good dog!') {
+            dogButton.innerText = 'Bad dog!'
+            dog.isGoodDog = true
+        } else {
+            dogButton.innerText = 'Good dog!'
+            dog.isGoodDog = false
+        }
+        updateDatabase(dog)
+    }
+
     dogInfoDiv.append(dogImg, dogName, dogButton)
+}
+
+// step 4: toggle good dog/bad dog
+function updateDatabase(dog) {
+    fetch(dogUrl + '/' + dog.id, {
+        method: 'PATCH',
+        headers: {
+            'Content-Type': 'application/JSON',
+            'Accept': 'application/JSON'
+        },
+        body: JSON.stringify({
+            isGoodDog: dog.isGoodDog
+        })
+    })
 }
