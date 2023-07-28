172ab3d6408e508968e09ad39dffa1a027109693
BreElle Wells <breellejordyn@gmail.com>
2023-07-27 21:39:26 -0600

    render dog info on click

--- src/index.js
+++ src/index.js
@@ -1,18 +1,40 @@
 const dogUrl = 'http://localhost:3000/pups'
 
-const dogDiv = document.getElementById('dog-bar')
+const dogNameDiv = document.getElementById('dog-bar')
 
 // step 2: add pups to dog bar
 fetch (dogUrl)
 .then(r => r.json())
-.then(data => renderDogs(data))
+.then(data => renderDogNames(data))
 
-function renderDogs(dogs) {
+function renderDogNames(dogs) {
     dogs.forEach((singleDog) => {
         const dogName = document.createElement('span')
         dogName.innerText = singleDog.name
+        dogName.classList.add('dog-name')
+        dogName.onclick = () => renderDogInfo(singleDog)
 
-        dogDiv.append(dogName)
+        dogNameDiv.append(dogName)
     })
 }
 
+// step 3: show pup info in dog info div
+const dogInfoDiv = document.getElementById('dog-summary-container')
+
+function renderDogInfo(dog) {
+    dogInfoDiv.innerText = ""
+    const dogImg = document.createElement('img')
+    dogImg.src = dog.image
+
+    const dogName = document.createElement('h2')
+    dogName.innerText = dog.name
+
+    const dogButton = document.createElement('button')
+    if (dog.isGoodDog) {
+        dogButton.innerText = 'Good dog!'
+    } else {
+        dogButton.innerText = 'Bad dog!'
+    }
+
+    dogInfoDiv.append(dogImg, dogName, dogButton)
+}
